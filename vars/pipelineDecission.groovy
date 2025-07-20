1de970f190b8bc25fa2cf2b1078eb3fdc688f5f5
Muralisanku <sankumurali4a6@gmail.com>
2025-07-20 14:01:57 +0530

    jenkins

---
+++ vars/pipelineDecission.groovy
@@ -0,0 +1,18 @@
+def decissionPipeline(Map configMap){
+    application = configMap.get("application")
+    switch(application) {
+        case 'nodejsVM'
+            nodejsVM(configMap)
+            break
+        case 'javaVM'
+            javaVM(configMap)
+            break
+        case 'nodejsEKS'
+            nodejsEKS(configMap)
+            break
+        default:
+            error "Application is not recongnised"
+            break
+            
+    }
+}
